d57236d10f06e1daecd2634eb9816a07e95dd527
Rishabh Jain <69865046+jennyrishabh@users.noreply.github.com>
2021-06-05 22:08:34 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>javascipt &amp; React.js</h1>
+      <p>i m learning this react &amp; javascipt programme ..!!</p>
+    </div>
+  );
+}
+
+export default Note;
